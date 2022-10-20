c8e41b96c7b0decf643fd9c421c377df34d020e3
stylishbot <74144478+stylishbot@users.noreply.github.com>
2022-10-21 00:25:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ try:
     companies = st.multiselect(
         '会社名を選択してください。',
         list(df.index),
-        ['google', 'amazon', 'facebook', 'apple']
+        ['google', 'amazon', 'apple']
     )
 
     if not companies:
