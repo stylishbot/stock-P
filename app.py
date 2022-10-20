48b50db4d2fe6b989fc9b5e172c07c67a5ba732b
stylishbot <74144478+stylishbot@users.noreply.github.com>
2022-10-21 00:17:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def get_data(days, tickers):
     for company in tickers.keys():
         tkr = yf.Ticker(tickers[company])
         hist = tkr.history(period=f'{days}d')
-        hist.index = hist.index.strftime('%d %B %Y')
         hist = hist[['Close']]
         hist.columns = [company]
         hist = hist.T
